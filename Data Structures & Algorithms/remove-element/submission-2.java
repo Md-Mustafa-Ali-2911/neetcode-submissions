5abfaf4250f2a9d66ac46b1b6509368e0175c790
MD MUSTAFA ALI <116714683+Md-Mustafa-Ali-2911@users.noreply.github.com>
2026-06-14 15:48:21 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        for (int i = 0; i < right; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        return left;
+    }
+}
